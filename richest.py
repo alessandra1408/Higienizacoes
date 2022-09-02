177ead308c78e5c3eddf655c937a548471516d0d
alessandra1408 <alessandraoitnb@gmail.com>
2022-09-02 11:15:05 -0300

    first commit

---
+++ richest.py
@@ -0,0 +1,14 @@
+import pandas as pd #para ler, visualizar e printar infos do df
+import numpy as np #numpy porque é sempre bom importar numpy né 
+import csv
+
+df = pd.read_csv('copy.csv')
+
+with open('saida.csv', 'w') as saida:
+    writer = csv.writer(saida)
+    df['NetWorth'] = df['NetWorth'].replace('$','')
+    df['NetWorth'] = df['NetWorth'].replace('"','')
+    writer.writerow(['Name','NetWorth','Age','Country/Territory','Source','Industry'])
+
+    for col in list(df.columns):
+        writer.writerow(df[col])
