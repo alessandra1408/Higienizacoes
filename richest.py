b06bd2e92ae923377fdbfb048cbeb1afe830c542
alessandra1408 <alessandraoitnb@gmail.com>
2022-09-02 11:18:45 -0300

    Exclui csv copy

--- richest.py
+++ richest.py
@@ -2,7 +2,7 @@ import pandas as pd #para ler, visualizar e printar infos do df
 import numpy as np #numpy porque é sempre bom importar numpy né 
 import csv
 
-df = pd.read_csv('copy.csv')
+df = pd.read_csv('TopRichestInWorld.csv')
 
 with open('saida.csv', 'w') as saida:
     writer = csv.writer(saida)
